97138d00d18828254cb0bbb65277908df862abb8
danielb07 <danielbarraud07@gmail.com>
2020-05-03 19:42:34 +0530

    Used angle property for the head part of the body so that it can be drawn at different angles-WhJ-feedback

--- js/cannon.js
+++ js/cannon.js
@@ -1,8 +1,21 @@
-class Cannon extends CanonBall {
-    constructor(x,y,width,height){
-        super(x,y,width,height)
-    }
-    display(){
-        super.display();
-    }
+class Cannon  {
+    constructor(x,y,radius,angle){
+        var option = {
+            isStatic:true
+        }
+        this.body = Bodies.circle(x,y,radius,option);
+        this.radius = radius;
+        Matter.Body.setAngle(this.body, angle)
+        World.add(world,this.body); 
+     }
+     display(){
+       var pos = this.body.position;
+       var angle = this.body.angle;
+       push();
+       translate(pos.x,pos.y)
+       rotate(angle);
+       ellipseMode(CENTER);
+       ellipse(0,0,this.radius);
+       pop();
+     }
 } 
--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ const { Engine, World, Bodies, Body, Mouse, MouseConstraint, Constraint, Composi
 // The above line creates different constant variables for Engine, World, Bodies etc.
 var engine, world
 var tank, ball, shoot, core;
+var launcherangle = 3/2
 
 function setup() {
     createCanvas(800,400)
@@ -13,9 +14,9 @@ function setup() {
     Lowertank = new Tanker(124,370,100,20);
     Uppertank = new Tanker(124,350,100,20);
     ball = new Ball(174,150,100)
-    core = new Cannon(175,345,80);
+    core = new Cannon(175,345,80,PI/4);
     ground = new Ground(10,390,800,20)
-    lancher = new Lancher(189,320,100,20,-PI/7);
+    lancher = new Lancher(175,320,100,20,-PI/launcherangle);
 }
 
 function draw() {
